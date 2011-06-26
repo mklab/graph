f1fc5c339e30ae37cf94097f982522c7b82d58f2
ishikura <ishikura@ec85b95f-da66-0410-9fa5-fe9ccd2450a3>
2011-06-26 01:06:09 +0000

    Improved grid generation algorithm.
    git-svn-id: svn+ssh://birdcage.mk.ces.kyutech.ac.jp/home/mutsuki/ishikura/svn/graph/trunk@410 ec85b95f-da66-0410-9fa5-fe9ccd2450a3

--- src/main/java/org/mklab/ishikura/graph/g2d/GridFactoryImpl.java
+++ src/main/java/org/mklab/ishikura/graph/g2d/GridFactoryImpl.java
@@ -16,21 +16,86 @@ class GridFactoryImpl implements GridFactory {
   public Grid create(double start, double end) {
     if (start > end) throw new IllegalArgumentException(start + " > " + end); //$NON-NLS-1$
 
-    int interval = (int)(end - start + 0.5);
-    interval = interval / 5;
-    interval = interval / 10 * 10;
-    if (interval == 0) interval = 1;
-
+    double interval = fix(getGridInterval(end - start));
     final Grid grid = new Grid();
 
-    int n = (int)start / interval * interval;
-    if (n < start) n += interval;
-    while (n <= end) {
+    double n = (int)(start / interval) * interval;
+    n = fix(n);
+
+    if (n < start) n = fix(n + interval);
+    while (n < end) {
       grid.add(n);
-      n += interval;
+      n = fix(n + interval);
     }
 
     return grid;
+  }
+
+  /**
+   * 精度による問題を修正します。
+   * <p>
+   * ex) 999.9999999999...=> 1000
+   * 
+   * @param d 値
+   * @return 修正した値
+   */
+  // FIXME 近似の仕方が分からない
+  @SuppressWarnings("boxing")
+  private double fix(double d) {
+    return Double.parseDouble(String.format("%f", d)); //$NON-NLS-1$
+  }
+
+  /**
+   * 与えられた値を0~29の値にするために必要な乗数を計算します。
+   * 
+   * @param l 値
+   * @return 0~29の値にするために掛ける必要がある値
+   */
+  double getMultiplierForMakingNumberBetween0_29(double l) {
+    double length = l;
+    double multiplier = 1;
+    if (length < 0) {
+      length *= -1;
+      multiplier *= -1;
+    }
+    while (length >= 30) {
+      length /= 10;
+      multiplier /= 10;
+    }
+    while (length < 3) {
+      length *= 10;
+      multiplier *= 10;
+    }
+    return multiplier;
+  }
+
+  double getGridInterval(double d) {
+    double n = d;
+
+    final double mul = getMultiplierForMakingNumberBetween0_29(d);
+    n *= mul;
+    double gridInterval = mapGridInterval(n);
+    gridInterval /= mul;
+
+    return fix(gridInterval);
+  }
+
+  /**
+   * グリッド間隔を取得します。
+   * 
+   * @param d 3~29までの値
+   * @return グリッド間隔
+   */
+  double mapGridInterval(double d) {
+    if (d < 3) throw new IllegalArgumentException(d + " < " + 3); //$NON-NLS-1$
+    if (d >= 30) throw new IllegalArgumentException(d + " >= " + 30); //$NON-NLS-1$
+
+    if (d < 6) return 1;
+    if (d < 12) return 2;
+    if (d < 15) return 4;
+    if (d < 30) return 5;
+
+    throw new IllegalArgumentException();
   }
 
 }
---
+++ src/main/java/org/mklab/ishikura/graph/g2d/GridFactoryImplTest.java
@@ -0,0 +1,151 @@
+/**
+ * 
+ */
+package org.mklab.ishikura.graph.g2d;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+
+/**
+ * @author Yuhi Ishikura
+ * @version $Revision$, 2011/06/25
+ */
+public class GridFactoryImplTest {
+
+  static GridFactoryImpl gridFactory = new GridFactoryImpl();
+
+  /**
+   * Test method for
+   * {@link org.mklab.ishikura.graph.g2d.GridFactoryImpl#getMultiplierForMakingNumberBetween0_29(double)}
+   * .
+   */
+  @Test
+  public void testGetMultiplierForMakingNumberBetween0_29() {
+    for (double i = 3; i < 30; i += 0.1) {
+      assertEquals(1, gridFactory.getMultiplierForMakingNumberBetween0_29(i), 1e-10);
+    }
+    for (double i = 30; i < 300; i += 0.1) {
+      assertEquals(0.1, gridFactory.getMultiplierForMakingNumberBetween0_29(i), 1e-10);
+    }
+    for (double i = 300; i < 3000; i += 0.1) {
+      assertEquals(0.01, gridFactory.getMultiplierForMakingNumberBetween0_29(i), 1e-10);
+    }
+
+    for (double i = -3; i > -30; i -= 0.1) {
+      assertEquals(-1, gridFactory.getMultiplierForMakingNumberBetween0_29(i), 1e-10);
+    }
+    for (double i = -30; i > -300; i -= 0.1) {
+      assertEquals(-0.1, gridFactory.getMultiplierForMakingNumberBetween0_29(i), 1e-10);
+    }
+    for (double i = -300; i > 3000; i -= 0.1) {
+      assertEquals(-0.01, gridFactory.getMultiplierForMakingNumberBetween0_29(i), 1e-10);
+    }
+
+    for (double i = 0.3; i <= 2.9; i += 0.01) {
+      assertEquals(10, gridFactory.getMultiplierForMakingNumberBetween0_29(i), 1e-10);
+    }
+    for (double i = 0.03; i <= 0.029; i += 0.01) {
+      assertEquals(100, gridFactory.getMultiplierForMakingNumberBetween0_29(i), 1e-10);
+    }
+    for (double i = 0.003; i <= 0.0029; i += 0.01) {
+      assertEquals(1000, gridFactory.getMultiplierForMakingNumberBetween0_29(i), 1e-10);
+    }
+    assertEquals(0.1, gridFactory.getMultiplierForMakingNumberBetween0_29(290.1), 1e-10);
+  }
+
+  /**
+   * Test method for
+   * {@link org.mklab.ishikura.graph.g2d.GridFactoryImpl#mapGridInterval(double)}
+   * .
+   */
+  @Test
+  public void testMapGridInterval() {
+    for (double d = 3; d <= 5; d += 0.1) {
+      assertEquals(1, gridFactory.mapGridInterval(d), 1e-10);
+    }
+    for (double d = 6; d <= 11; d += 0.1) {
+      assertEquals(2, gridFactory.mapGridInterval(d), 1e-10);
+    }
+    for (double d = 12; d <= 14; d += 0.1) {
+      assertEquals(4, gridFactory.mapGridInterval(d), 1e-10);
+    }
+    for (double d = 15; d <= 29; d += 0.1) {
+      assertEquals(5, gridFactory.mapGridInterval(d), 1e-10);
+    }
+  }
+
+  /**
+   * Test method for
+   * {@link org.mklab.ishikura.graph.g2d.GridFactoryImpl#getGridInterval(double)}
+   * .
+   */
+  @Test
+  public void testGetGridInterval() {
+    for (double d = 3; d <= 5; d += 0.1) {
+      assertEquals(1, gridFactory.getGridInterval(d), 1e-10);
+    }
+    for (double d = 6; d <= 11; d += 0.1) {
+      assertEquals(2, gridFactory.getGridInterval(d), 1e-10);
+    }
+    for (double d = 12; d <= 14; d += 0.1) {
+      assertEquals(4, gridFactory.getGridInterval(d), 1e-10);
+    }
+    for (double d = 15; d <= 29; d += 0.1) {
+      assertEquals(5, gridFactory.getGridInterval(d), 1e-10);
+    }
+
+    for (double d = 30; d < 60; d += 0.1) {
+      assertEquals(10, gridFactory.getGridInterval(d), 1e-10);
+    }
+    for (double d = 60; d < 120; d += 0.1) {
+      assertEquals(20, gridFactory.getGridInterval(d), 1e-10);
+    }
+    for (double d = 120; d < 150; d += 0.1) {
+      assertEquals(40, gridFactory.getGridInterval(d), 1e-10);
+    }
+    for (double d = 150; d < 300; d += 0.1) {
+      assertEquals(50, gridFactory.getGridInterval(d), 1e-10);
+    }
+
+    assertEquals(0.2, gridFactory.getGridInterval(0.3674 - (-0.3379)), 1e-10);
+  }
+
+  /**
+   * Test method for
+   * {@link org.mklab.ishikura.graph.g2d.GridFactoryImpl#create(double, double)}
+   * .
+   */
+  @Test
+  public void testCreate() {
+    assertGridEquals(grid(-0.2, 0, 0.2), gridFactory.create(-0.3379, 0.3647));
+    assertGridEquals(grid(-100000, 0, 100000), gridFactory.create(-154077.167222, 156115.562485));
+    assertGridEquals(grid(-0.6, -0.4, -0.2, 0), gridFactory.create(-0.681267, 0.165232));
+  }
+
+  private Grid grid(double... d) {
+    Grid grid = new Grid();
+    for (double dd : d) {
+      grid.add(dd);
+    }
+    return grid;
+  }
+
+  private void assertGridEquals(Grid expected, Grid actual) {
+    assertArrayEquals(getGridElements(expected), getGridElements(actual));
+  }
+
+  private Double[] getGridElements(Grid g) {
+    List<Double> grids = new ArrayList<Double>();
+    for (Double d : g) {
+      grids.add(d);
+    }
+    return grids.toArray(new Double[grids.size()]);
+  }
+
+}
